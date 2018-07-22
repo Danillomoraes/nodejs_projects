e1d25c209e6e31939e8181649096a89026911541
danillo.moraes <danillo.moraes@outlook.com>
2018-07-22 19:24:20 -0300

    lecture 16 part 2

--- helloworld/app.js
+++ helloworld/app.js
@@ -1,27 +1,25 @@
 console.log('Starting app');
 
 const fs = require('fs');
-const os = require('os');
-const notes = require('./notes.js');
 const _ = require('lodash');
+const yargs = require ('yargs');
 
-//console.log(_.isString(true));
-//console.log(_.isString('Danillo'));
+const notes = require('./notes.js');
 
-var filteredArray = _.uniq(['Danillo',1,'Danillo',1,2,3,4,5]);
-console.log(filteredArray);
+const argv = yargs.argv;
+var command = argv._[0];
 
-var res = notes.addFunc(1,2);
+console.log('command: ', command);
+console.log('Yargs', argv);
 
-console.log(res);
-
-
-
-// var user = os.userInfo();
-// console.log(user);
-//
-// fs.appendFile('greetings.txt', `Hello World!  ${user.username} !`, function (err) {
-//   if (err) {
-//     console.log('unable to write');
-//   }
-// });
+if (command=== 'add') {
+  notes.addNote(argv.title, argv.body);
+} else if (command==='list'){
+  notes.getAll();
+} else if(command === 'remove') {
+  notes.removeNote(argv.title);
+} else if (command === 'read'){
+  notes.getNote(argv.title)
+} else {
+  console.log('Command not recognized');
+}
--- helloworld/notes.js
+++ helloworld/notes.js
@@ -1,9 +1,43 @@
 console.log("Starting notes.js");
 
-module.exports.addNote = function () {
-  return 'New Note'
+const fs = require('fs');
+
+var addNote = (title, body) => {
+  var notes = [];
+  var note = {
+    title,
+    body
+  };
+
+  try{
+    var notesString = fs.readFileSync('notes-data.json');
+    notes = JSON.parse(notesString);
+  }catch(e){
+
+  }
+
+  
+
+  notes.push(note);
+  fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+
+};
+
+var getAll = () => {
+  console.log('getting all notes');
+};
+
+var getNote = (title) => {
+  console.log('return note', title);
 }
 
-module.exports.addFunc = function (a, b) {
-  return a + b
+var removeNote = (title) => {
+  console.log('removing note: ', title );
+}
+
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote
 }
