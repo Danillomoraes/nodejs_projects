631d52e65227bb524b894e8490fb2292af46b2a6
danillo.moraes <danillo.moraes@outlook.com>
2018-07-22 19:23:47 -0300

    lecture 16

--- helloworld/app.js
+++ helloworld/app.js
@@ -3,6 +3,13 @@ console.log('Starting app');
 const fs = require('fs');
 const os = require('os');
 const notes = require('./notes.js');
+const _ = require('lodash');
+
+//console.log(_.isString(true));
+//console.log(_.isString('Danillo'));
+
+var filteredArray = _.uniq(['Danillo',1,'Danillo',1,2,3,4,5]);
+console.log(filteredArray);
 
 var res = notes.addFunc(1,2);
 
