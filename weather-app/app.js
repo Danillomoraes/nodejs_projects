a2dc61c71f000127e06a12eb3dd3b7adbc14ba6a
danillo.moraes <danillo.moraes@outlook.com>
2018-07-24 21:36:04 -0300

    lecture 29 weather-app

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,1 +1,9 @@
-console.log('i dont know what im doing with my life');
+//console.log('i dont know what im doing with my life');
+const request = require('request');
+
+request({
+  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=59%20rua%20terra%20roxa%20cajamar',
+  json: true
+}, (error, response, body) => {
+console.log(JSON.stringify(body, undefined, 2));
+});
