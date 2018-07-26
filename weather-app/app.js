29e09e53806b474b04e32f67bce24f9c2b5db090
danillo.moraes <danillo.moraes@outlook.com>
2018-07-26 17:11:27 -0300

    lecture 34 weather-app

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,52 @@
 //console.log('i dont know what im doing with my life');
-const request = require('request');
+const geocode = require('./geocode/geocode');
+const yargs = require('yargs');
+const request =require('request');
 
-request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=59%20rua%20terra%20roxa%20cajamar',
-  json: true
-}, (error, response, body) => {
-console.log(JSON.stringify(body, undefined, 2));
+var darkSecret = "c5876f20dbf92c97d78eb1425601206f";
+
+const argv = yargs.options ({
+  address: {
+    demand: true,
+    alias: 'a',
+    describe: 'Address to fetch weather',
+    string: true
+  }
+})
+.help()
+.alias("help", "h")
+.argv;
+
+var r = undefined;
+
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if (errorMessage) {
+    console.log(errorMessage);
+  } else {
+    console.log(JSON.stringify(results, undefined, 2));
+
+    var long = results.Longitude;
+    var late = results.Latitude;
+    var url = `https://api.darksky.net/forecast/${darkSecret}/${late},${long}?lang=pt&si=temperature`;
+
+    console.log(url);
+
+    request({
+      rejectUnauthorized: false,
+      url: url,
+      json: true,
+      proxy: "http://CPTM%5Cdanillom:Cptm0004@iraque.cptm.info:80/"
+
+    }, (error, response, body) => {
+      if (error) {
+        console.log(error);
+      }else {
+        console.log(JSON.stringify(body, undefined, 2));
+        //console.log(body.currently);
+      }
+    })
+  }
 });
+
+
+// console.log(key);
---
+++ weather-app/geocode/geocode.js
@@ -0,0 +1,59 @@
+const request = require('request');
+const fs = require('fs');
+
+var fetchKey = () => {
+  try {
+    var keyString = fs.readFileSync('google.json');
+    return JSON.parse(keyString);
+  } catch (e) {
+    console.log('failed read key');
+    return false
+  } finally {
+
+  }
+}
+
+
+var geocodeAddress = function (address, callback)  {
+
+  var key = fetchKey().key
+  var urlg = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${key}`
+
+if (key) {
+
+  request({
+    url: urlg,
+    json: true,
+    proxy: "http://CPTM%5Cdanillom:Cptm0004@iraque.cptm.info:80/"
+  }, (error, response, body) => {
+
+    if(error){
+      callback("Unable to connect to internet")
+    }else if (body.status === "ZERO_RESULTS") {
+      callback("None Adress Founc")
+    }else if(body.status === "OK"){
+
+      var lat = body.results[0].geometry.location.lat;
+      var lng = body.results[0].geometry.location.lng;
+
+      callback(undefined, {
+        Address:body.results[0].formatted_address,
+        Latitude:lat,
+        Longitude:lng
+      });
+
+
+    }else{
+      console.log(JSON.stringify(body, undefined, 2));
+      console.log(error);
+      //console.log(response);
+
+      }
+
+    });
+  }
+}
+
+module.exports = {
+  geocodeAddress
+}
