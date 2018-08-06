b961d7621d95db053b2ad96b02a1aa8b74b14c96
danillo.moraes <danillo.moraes@outlook.com>
2018-08-06 15:26:50 -0300

    lecture 46

---
+++ node-web-server/server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname+'/public'));
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method}, ${req.url}`
+  console.log(log);
+
+  fs.appendFile('server.log', log +'\n', (err)=> {
+    if (err) {
+      console.log('unable to append to server.log');
+    }
+  })
+
+  next();
+});
+
+hbs.registerHelper('getcurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screanIt', (text) => {
+  return text
+})
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home page',
+    welcomeMessage: 'dont break my heart',
+    currentYear: new Date().getFullYear()
+  })
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errosMessage: "unable to feed"
+  })
+})
+
+
+
+app.listen(3000, () => {
+  console.log('server is up on port 3000');
+});
